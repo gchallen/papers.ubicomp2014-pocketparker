e38f4fd36337ef83ebbceefbade835f91d800fdb
Geoffrey Challen <challen@buffalo.edu>
2013-04-05 18:03:18 -0400

    New.

--- figures/rates.py
+++ figures/rates.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
-import argparse, sys
+import argparse, sys, numpy
 from matplotlib import rc
+from scipy import misc
 
 rc('font',**{'family':'serif','serif':['Times'], 'size': 14})
 rc('text', usetex=True)
@@ -14,16 +15,22 @@ parser.add_argument('rates', type=int, nargs='+', help='Rates to display.')
 parser.add_argument('--capacity', type=int, default=200, help='Actual rate. Default is 200.')
 args = parser.parse_args()
 
-print args.rates
-sys.exit()
-
 fig = plt.figure()
 ax = fig.add_subplot(111)
-plt.title("\\textbf{Monitored Capacity Estimation}")
-ax.set_xlabel("\\textbf{Day}")
-ax.set_ylabel("\\textbf{Running Count $a_l$}")
-for lot in lots.lots:
-  lot_counts[lot.name] = [(t / 60. / 60. / 24., count) for t, count in lot_counts[lot.name]]
-  ax.plot(*zip(*lot_counts[lot.name]), label="Lot %d" % (int(lot.name[-1]),))
-ax.legend(fontsize=8)
+plt.title("\\textbf{Rate Estimation}")
+ax.set_xlabel("\\textbf{Scaled Rate}")
+ax.set_ylabel("\\textbf{Probability}")
+
+for rate in args.rates:
+  probs = []
+  monitored = (float(rate) / args.capacity)
+  prob_sum = 0.
+  for i in numpy.arange(rate, args.capacity * 2, 1):
+    prob = misc.comb([i], [rate])[0] * (monitored ** rate) * ((1. - monitored) ** (i - rate))
+    prob_sum += prob
+    probs.append((i, prob))
+  probs = [(i, prob / prob_sum) for i, prob in probs]
+  ax.plot(*zip(*probs), label="%d Observed, %.2f Monitored Fraction" % (rate, monitored,))
+
+ax.legend(fontsize=10)
 plt.savefig(args.output,bbox_inches='tight')
